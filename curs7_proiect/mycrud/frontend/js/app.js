fe3dc81fce06c0e4536f97d2fa6cfb1e7f453e63
Adela Mag <adela_mag@yahoo.com>
2025-05-12 14:34:56 +0300

    Finalizare proiect + recap

--- curs7_proiect/mycrud/frontend/js/app.js
+++ curs7_proiect/mycrud/frontend/js/app.js
@@ -43,7 +43,7 @@ $(document).ready(function ($) {
         //console.log(data);
         $('#example').dataTable({
             "paging": true,
-            "pageLength": 2,
+            "pageLength": 10,
             processing: true,
             serverSide: false,
             "data": data.data,
@@ -63,7 +63,7 @@ $(document).ready(function ($) {
                     render: function (data, type, row, meta) {
                         if (type === 'display') {
                             let link = 'http://localhost:3002/users/' + row.id;
-                            return `<a href="javascript:void(0)" class="btn btn-info view" data-id="` + row.id + `">Vezi</a>
+                            return `<a href="javascript:void(0)" class="btn btn-success view" data-id="` + row.id + `">Vezi</a>
                             <a href="javascript:void(0)" class="btn btn-primary edit" data-id="`+ row.id + `">Editeaza</a>
                             <a href="javascript:void(0)" class="btn btn-danger delete" data-id="`+ row.id + `">Sterge</a>`;
                         }
@@ -143,8 +143,8 @@ $(document).ready(function ($) {
         else {
             idValue
         }
-        alert(method);
-        alert(urlReq);
+        // alert(method);
+        // alert(urlReq);
         $.ajax({
 
             type: method,
